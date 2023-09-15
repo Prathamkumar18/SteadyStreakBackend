ec4437350eea99906822e461ffc361fcd1487473
Prathamkumar18 <pratham854305@gmail.com>
2023-09-15 23:26:41 +0530

    ip

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const db=require('./db');
+const ip='0.0.0.0';
 
 const app = express();
 const port = process.env.PORT || 8082;
@@ -11,6 +12,6 @@ app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 
-app.listen(port, () => {
+app.listen(port,ip, () => {
   console.log(`Server is running on port ${port}`);
 });
